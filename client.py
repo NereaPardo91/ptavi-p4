f41c35af71a131a34f2c5a03d45450cc48581c2a
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-10-24 09:35:13 +0200

    Comprension del programa

--- client.py
+++ client.py
@@ -4,19 +4,19 @@
 Programa cliente UDP que abre un socket a un servidor
 """
 
-import socket
+import socket  	#importacion del modulo socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
+SERVER = 'localhost'		#variables constantes
 PORT = 6001
 LINE = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((SERVER, PORT))	#nos conectamos a un servidor
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')	#enviamos una secuencia de bytes por el socket
+    data = my_socket.recv(1024)	#leemos del socket
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
