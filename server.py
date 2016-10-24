0b2b7058d8316525d77fddc05673a18710f389bc
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-10-24 17:37:23 +0200

    PUNTO 6 ACABADO

--- server.py
+++ server.py
@@ -8,20 +8,29 @@ import socketserver    #importamos el modulo
 import sys
 
 P = int(sys.argv[1])
-class EchoHandler(socketserver.DatagramRequestHandler): #unica clase que maneja las peticiones
+
+class SIPRegisterHandler(socketserver.DatagramRequestHandler): #unica clase que maneja las peticiones
+
+    list_users = {}
     """
     Echo server class
     """
 
     def handle(self):   #metodo que se ejcuta cada vez que recibimos una peticion 
-        self.wfile.write(b"Hemos recibido tu peticion")
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-            print("IP:", self.client_address[0])
-            print("Puerto:", self.client_address[1])
+        #self.wfile.write(b"Hemos recibido tu peticion")
+        Message = self.rfile.read().decode('utf-8')
+        print("REGISTER recibido " + Message)
+        Direccion_IP = self.client_address[0]
+        Direccion_SIP = Message.split(':')[1].split(' ')[0]
+        #print("traza: " + Direccion_SIP)
+        #print("traza: " + Direccion_IP)
+        self.list_users[Direccion_SIP] = Direccion_IP
+        print(self.list_users)
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
             
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', P), EchoHandler)
+    serv = socketserver.UDPServer(('', P), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
